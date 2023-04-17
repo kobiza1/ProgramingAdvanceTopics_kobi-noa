7e1169e0e088c4894cb9cf0641cc0872f3ce2864
kobiza1 <kobiza@post.bgu.ac.il>
2023-04-17 23:04:43 +0300

    ב

--- ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
+++ ATP-Project-PartA/src/algorithms/search/SearchableMaze.java
@@ -37,52 +37,52 @@ public class SearchableMaze implements ISearchable{
                 && (maze_board[row+1][column] == 0 || maze_board[row][column+1] == 0)){
             next_position = new Position(row + 1, column + 1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(15, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 15, next_position, cur_state, key));
         }
         // case 2 : move right and up (slant)
         if(column != (maze_board[0].length - 1) && row != 0 && maze_board[row-1][column+1] == 0
                 && (maze_board[row-1][column] == 0 || maze_board[row][column+1] == 0)){
             next_position = new Position(row - 1, column + 1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(15, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 15, next_position, cur_state, key));
         }
         // case 3 : move left and up (slant)
         if(column != 0 && row != 0 && maze_board[row-1][column-1] == 0
                 && (maze_board[row-1][column] == 0 || maze_board[row][column-1] == 0)){
             next_position = new Position(row - 1, column - 1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(15, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 15, next_position, cur_state, key));
         }
         // case 4 : move left and down (slant)
         if(column != 0 && row != (maze_board.length-1) && maze_board[row+1][column-1] == 0
                 && (maze_board[row+1][column] == 0 || maze_board[row][column-1] == 0)){
             next_position = new Position(row + 1, column - 1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(15, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 15, next_position, cur_state, key));
         }
         // case 5 : move right
         if((column != (maze_board[0].length - 1)) && maze_board[row][column+1] == 0){
             next_position = new Position(row, column+1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(10, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 10, next_position, cur_state, key));
         }
         // case 6 : move left
         if(column != 0 && maze_board[row][column-1] == 0){
             next_position = new Position(row, column-1);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(10, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 10, next_position, cur_state, key));
         }
         // case 7 : move up
         if(row != 0 && maze_board[row-1][column] == 0){
             next_position = new Position(row - 1, column);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(10, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 10, next_position, cur_state, key));
         }
         // case 8 : move down
         if(row != (maze_board.length - 1) && maze_board[row+1][column] == 0){
             next_position = new Position(row + 1, column);
             Integer key = next_position.getRowIndex()*m_maze.getColumns_number() + next_position.getColumnIndex();
-            all_possible_states.add(new MazeState(10, next_position, cur_state, key));
+            all_possible_states.add(new MazeState(cur_state.getCost() + 10, next_position, cur_state, key));
         }
 
         return all_possible_states;
--- ATP-Project-PartA/src/test/RunSearchOnMaze.java
+++ ATP-Project-PartA/src/test/RunSearchOnMaze.java
@@ -5,19 +5,30 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(1000, 1000);
-        SearchableMaze searchableMaze = new SearchableMaze(maze);
-        solveProblem(searchableMaze, new BreadthFirstSearch()); solveProblem(searchableMaze, new DepthFirstSearch()); solveProblem(searchableMaze, new BestFirstSearch());
+        boolean check = true;
+//        solveProblem(searchableMaze, new BreadthFirstSearch()); solveProblem(searchableMaze, new DepthFirstSearch()); solveProblem(searchableMaze, new BestFirstSearch());
+        while (check)
+        {
+
+            Maze maze = mg.generate(1000, 1000);
+            System.out.println(String.format("Maze generation time(ms): %s", mg.measureAlgorithmTimeMillis(1000/*rows*/,1000/*columns*/)));
+            SearchableMaze searchableMaze = new SearchableMaze(maze);
+            check = solveProblem(searchableMaze, new BreadthFirstSearch());
+            check = check && solveProblem(searchableMaze, new DepthFirstSearch());
+            check = check && solveProblem(searchableMaze, new BestFirstSearch());
+        }
     }
-    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
+    private static boolean solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
         //Solve a searching problem with a searcher
         Solution solution = searcher.solve(domain);
         System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
         //Printing Solution Path
         System.out.println("Solution path:");
         ArrayList<AState> solutionPath = solution.getSolutionPath();
-        for (int i = 0; i < solutionPath.size(); i++) {
+        /*for (int i = 0; i < solutionPath.size(); i++) {
             System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
-        }
+        }*/
+        System.out.println(solutionPath.size());
+        return solutionPath.size() != 0;
     }
 }
